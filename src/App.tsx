5127d0f25442047af4a01f2dd3dd36f7dd3c104f
Cauã Andrade <cauasampaiovolei@gmail.com>
2023-01-17 17:39:39 -0300

    Pronto

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import logo from  './assets/shuffle.svg'
+
+type filme = [{
+  title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+}]|""
 
 function App() {
+
+  const [filme, setFilme] = useState<filme>()
+
+
+  function gerarId(){
+    const randomizar = Math.random() * 1000
+    const numeroFinal = Math.round(randomizar)
+    return numeroFinal
+  }
+
+  async function getFilme(){
+    const apiURL = `https://api.themoviedb.org/3/movie/${gerarId()}?api_key=`.concat(process.env.REACT_APP_API_KEY?process.env.REACT_APP_API_KEY:"")
+    await fetch(apiURL)
+          .then((response) => response.json())
+          .then((value) => {
+            const {title, overview, popularity, poster_path} = value
+            if(title == undefined || overview == ""){
+              getFilme()
+            }
+            setFilme([{title, overview, popularity, poster_path}])
+          })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} width={130} />
+      <h2>Não sabe oque assistir ?</h2>
+        {filme != "" && filme?filme?.map(item => {
+          const {title, overview, popularity, poster_path} = item
+          return(
+            <div className='areaApi'  key={0}>
+              <h3>{title}</h3>
+              <div className="snopse">
+                <img src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/".concat(poster_path)} width={150} height={180} />
+                <p>
+                  {overview}
+                </p>
+              </div>
+            </div>
+          )
+        }):""}
+        <div className='areabuttons'>
+          <button onClick={async () => {
+            await getFilme()
+          }}><img src={logo} width={35} />Encontrar Filme</button>
+          <span>
+            Clique em "Encontrar Filmes" que traremos informações de algum filme para você assistir hoje
+          </span>
+        </div>
     </div>
   );
 }
